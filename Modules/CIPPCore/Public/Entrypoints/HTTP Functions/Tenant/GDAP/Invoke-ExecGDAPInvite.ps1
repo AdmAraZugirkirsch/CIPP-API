571b68aa755bb43f859f501950c915c975590cbe
AdmAraZugirkirsch <adm.ara@zugikirschnext.onmicrosoft.com>
2025-09-11 12:17:59 +0200

    Update Invoke-ExecGDAPInvite.ps1

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/GDAP/Invoke-ExecGDAPInvite.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/GDAP/Invoke-ExecGDAPInvite.ps1
@@ -44,7 +44,7 @@ function Invoke-ExecGDAPInvite {
             try {
                 $Step = 'Creating GDAP relationship'
                 $JSONBody = @{
-                    'displayName'        = "CIPP_$((New-Guid).GUID)"
+                    'displayName'        = $displayName
                     'accessDetails'      = @{
                         'unifiedRoles' = @($RoleMappings | Select-Object roleDefinitionId)
                     }
