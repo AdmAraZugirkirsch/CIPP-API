e1447244ffe915f2064056602303e42e70bbd566
AdmAraZugirkirsch <adm.ara@zugikirschnext.onmicrosoft.com>
2025-09-11 16:42:07 +0200

    Update Invoke-ExecGDAPInvite.ps1

--- Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/GDAP/Invoke-ExecGDAPInvite.ps1
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Tenant/GDAP/Invoke-ExecGDAPInvite.ps1
@@ -29,7 +29,7 @@ function Invoke-ExecGDAPInvite {
             $SecurityAdminId = '194ae4cb‐b126‐40b2‐bd5b‐6091b380977d'
             $SecurityOperatorId = '5f2222b1‐57c3‐48ba‐8ad5‐d4759f1fde6f'
             
-            $roleIds = $RoleMappings | ForEach-Object { $_.roleDefinitionId }
+            $roleIds = $RoleMappings | ForEach-Object { $_.roleDefinitionId -split " " }
 
             $onlySOC = ($roleIds.Count -eq 2 -and $roleIds -contains $SecurityAdminId -and $roleIds -contains $SecurityOperatorId)
 
