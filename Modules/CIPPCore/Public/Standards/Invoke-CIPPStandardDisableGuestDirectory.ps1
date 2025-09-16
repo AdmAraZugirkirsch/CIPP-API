f7aebb6520573840130ca012576f16832d9e89a5
AdmAraZugirkirsch <adm.ara@zugikirschnext.onmicrosoft.com>
2025-09-16 11:42:50 +0200

    Update Invoke-CIPPStandardDisableGuestDirectory.ps1

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardDisableGuestDirectory.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardDisableGuestDirectory.ps1
@@ -32,7 +32,7 @@ function Invoke-CIPPStandardDisableGuestDirectory {
     ##$Rerun -Type Standard -Tenant $Tenant -Settings $Settings 'DisableGuestDirectory'
 
     try {
-        $CurrentInfo = New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/policies/authorizationPolicy/authorizationPolicy' -tenantid $Tenant
+        $CurrentInfo = New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/policies/authorizationPolicy' -tenantid $Tenant
     }
     catch {
         $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
@@ -47,7 +47,7 @@ function Invoke-CIPPStandardDisableGuestDirectory {
         } else {
             try {
                 $body = '{guestUserRoleId: "2af84b1e-32c8-42b7-82bc-daa82404023b"}'
-                New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/authorizationPolicy/authorizationPolicy' -Type patch -Body $body -ContentType 'application/json'
+                New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/authorizationPolicy' -Type patch -Body $body -ContentType 'application/json'
                 Write-LogMessage -API 'Standards' -tenant $tenant -message 'Disabled Guest access to directory information.' -sev Info
             } catch {
                 $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
