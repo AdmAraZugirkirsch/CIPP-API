4e91152da0c2ac47624c2b10edff3be705849b27
AdmAraZugirkirsch <adm.ara@zugikirschnext.onmicrosoft.com>
2025-09-16 15:45:48 +0200

    Update Invoke-CIPPStandardDisableGuestDirectory.ps1

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardDisableGuestDirectory.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardDisableGuestDirectory.ps1
@@ -47,7 +47,7 @@ function Invoke-CIPPStandardDisableGuestDirectory {
         } else {
             try {
                 $body = '{guestUserRoleId: "2af84b1e-32c8-42b7-82bc-daa82404023b"}'
-                New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/authorizationPolicy' -Type patch -Body $body -ContentType 'application/json'
+                New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/policies/authorizationPolicy/authorizationPolicy' -Type patch -Body $body -ContentType 'application/json'
                 Write-LogMessage -API 'Standards' -tenant $tenant -message 'Disabled Guest access to directory information.' -sev Info
             } catch {
                 $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
